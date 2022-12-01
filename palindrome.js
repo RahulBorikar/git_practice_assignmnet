ac4ac924710f9bb358ef78d7bbda49301248d84e
Sourav01112 <chaudharysourav997@gmail.com>
2022-12-01 23:55:17 +0530

    resolved

--- palindrome.js
+++ palindrome.js
@@ -4,17 +4,15 @@ let reverse = "";
 
 flag = false;
 
-for(let i = string.length - 1; i >= 0; i--){
-    reverse += string[i];
+for (let i = string.length - 1; i >= 0; i--) {
+  reverse += string[i];
 }
 
-for(let i = 0; i < string.length; i++){
-    if(string[i] === reverse[i]){
-        flag = true;
-    }else{
-        flag = false;
-        break;
-    }
-}console.log(flag);
+for (let i = 0; i <= string.length - 1; i++) {
+  if (string[i] === reverse[i]) {
+    flag = true;
+  }
+}
+console.log(flag);
 
 
